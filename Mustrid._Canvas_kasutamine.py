92203e3ee9ac7d1591bce747715337e977a8ea57
alexeypaal <120027728+alexeypaal@users.noreply.github.com>
2023-03-27 12:30:26 +0300

    Update Mustrid._Canvas_kasutamine.py

--- Mustrid._Canvas_kasutamine.py
+++ Mustrid._Canvas_kasutamine.py
@@ -8,6 +8,61 @@ import tkinter as tk
 from math import *
 from random import *
 
+#Harjutus. Lipud
+#Eesti lipp
+def eesti ():
+    nupp.configure()
+    aken.create_rectangle(300,100, 30,30, fill="blue")
+    aken.create_rectangle(300,100, 30,60, fill="black")
+    aken.create_rectangle(300,100, 30,130, fill="white")
+def Bahama ():
+    nupp.configure()
+    aken.create_rectangle(310,100, 550,30,fill="#0fb8a1" )  
+    aken.create_rectangle(310,100, 550,60,fill="#c9c310" )  
+    aken.create_rectangle(310,100, 550,130,fill="#0fb8a1" ) 
+    aken.create_polygon(310,30, 310,130, 400,80, fill="black" ) 
+def Litva ():
+    nupp.configure()
+    aken.create_rectangle(600,100, 800,30, fill="yellow")
+    aken.create_rectangle(600,100, 800,60, fill="#026b12")
+    aken.create_rectangle(600,100, 800,130, fill="red")
+
+
+    #Create Chess Board
+def board():
+    for y in range(8):
+        for x in range(8):
+            x1=x*70
+            y1=y*70
+            x2=x1+70 
+            
+
+
+
+
+
+aken=Tk()
+aken.title("Tahvel")
+tahvel=Canvas(aken, width=600,height=600, background="white")
+k=7
+x0=0
+y0=0
+x1=600
+y1=600
+a=300
+r=(a**2+a**2)**(1/2)
+p=(a-r)
+for i in range(7):
+    x0+=p
+    y0+=p
+    x1-=p
+    y1-=p
+    tahvel.create_rectangle(x0,y0,x1,y1,width=1,outline="blue", fill="red")
+    tahvel.create_oval(x0,y0,x1,y1,width=1,outline="blue", fill="yellow")
+    p=r-a 
+    r=r-p 
+    a=((r)*sqrt(2))/2
+tahvel.grid()
 
 def oval():
     colors=["black",
@@ -36,81 +91,6 @@ def oval():
         y1-=p 
         tahvel2.create_oval(x0,y0,x1,y1, fill=choice(colors))
     tahvel2.grid()
-
-
-                       
-
-#Harjutus. Lipud
-#Eesti lipp
-
-def eesti ():
-    nupp.configure()
-    raam.create_rectangle(300,100, 30,30, fill="blue")
-    raam.create_rectangle(300,100, 30,60, fill="black")
-    raam.create_rectangle(300,100, 30,130, fill="white")
-def Bahama ():
-    nupp.configure()
-    raam.create_rectangle(310,100, 550,30,fill="#0fb8a1" )  
-    raam.create_rectangle(310,100, 550,60,fill="#c9c310" )  
-    raam.create_rectangle(310,100, 550,130,fill="#0fb8a1" ) 
-    raam.create_polygon(310,30, 310,130, 400,80, fill="black" ) 
-def Litva ():
-    nupp.configure()
-    raam.create_rectangle(600,100, 800,30, fill="yellow")
-    raam.create_rectangle(600,100, 800,60, fill="#026b12")
-    raam.create_rectangle(600,100, 800,130, fill="red")
-
-raam=Tk()
-raam.title("Tahvel")
-tahvel=Canvas(raam, width=600,height=600, background="white")
-k=7
-x0=0
-y0=0
-x1=600
-y1=600
-a=300
-r=(a**2+a**2)**(1/2)
-p=(a-r)
-for i in range(7):
-    x0+=p
-    y0+=p
-    x1-=p
-    y1-=p
-    tahvel.create_rectangle(x0,y0,x1,y1,width=1,outline="blue", fill="red")
-    tahvel.create_oval(x0,y0,x1,y1,width=1,outline="blue", fill="yellow")
-    p=r-a 
-    r=r-p 
-    a=((r)*sqrt(2))/2
-tahvel.grid()
-
-
-#Create Chess Board
-t= turtle.Turtle()
-t.pensize(2)
-t.speed(0)
-def draw_square():
-    for i in range(4):
-        t.forward(40)
-        t.right(90)
-    t.forward(40)
-for i in range(8):
-    t.penup()
-    t.goto(0, 40* i)
-    t.pendown()
-    for x in range(8):
-        if (x+i) % 2==0:
-            color="black"
-        else:
-            color="white"
-        t.fillcolor(color)
-        t.begin_fill()
-        draw_square()
-        t.end_fill()
-t.hideturtle()
-turtle.done()
-
-
-
 
 
 
@@ -155,8 +135,10 @@ raam=Canvas(aken,
             width=800,
             height=621,
             bg="white",)
+    
 
-nupp4=draw_square(aken,
+
+nupp4=board(aken,
             text="valgusfoor",
             bg="lightblue",          #цвет background
             fg="#4f1fd1",         #другой цвет
@@ -165,7 +147,17 @@ nupp4=draw_square(aken,
             activeforeground="red",
             height=3,    
             width=20,
-            command=draw_square)
+            command=board)
+nupp5=oval(aken,
+            text="Ringid",
+            bg="lightblue",          #цвет background
+            fg="#4f1fd1",         #другой цвет
+            font="Dubai 20",  #шрифт и размер
+            activebackground="green",
+            activeforeground="red",
+            height=3,    
+            width=20,
+            command=oval)
 
 
 
@@ -195,13 +187,24 @@ canvas.create_oval(30, 180, 70, 220, fill='green', outline='')  # Сигнал
 canvas.create_polygon(48, 225, 52, 225, 50, 220, fill='green')  # Стрелка вверх
 
 
-nupp4.pack()
-nupp3.pack()
-nupp2.pack()
-nupp.pack()
-raam.pack()
+
+nupp=Radiobutton(aken,text="Eesti lipp",command=eesti)
+nupp2=Radiobutton(aken,text="Bahama lipp",command=Bahama)
+nupp3=Radiobutton(aken,text="Litva lipp",command=Litva)
+nupp4=Radiobutton(aken,text="valgusfoor",command=board)
+nupp5=Radiobutton(aken,text="Ringid",command=oval)
+
+nupp4.grid(row=1,column=1)
+nupp3.grid(row=2,column=1)
+nupp2.grid(row=3,column=1)
+nupp.grid(row=4,column=1)
+raam.grid(row=5,column=1)
+nupp5.grid(row=6,column=1)
+
+
+
 aken.mainloop()
-root.mainloop()
+
 
 
 
